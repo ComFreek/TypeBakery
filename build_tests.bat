c2368c1be209f60785cac1268159317e2979992c
ComFreek <comfreek@outlook.com>
2012-10-23 10:13:59 -0700

    Create gh-pages branch via GitHub

--- build_tests.bat
+++
@@ -1,5 +0,0 @@
-tsc EventHandler/EventHandler.ts EventHandler/EventHandler-UT.ts UnitTest/UnitTest.ts UnitTest/RunUnitTests.ts --out build/all.js
-tsc EventHandler/EventHandler.ts EventHandler/EventHandler-UT.ts UnitTest/UnitTest.ts UnitTest/RunUnitTests.ts --out UnitTest/all.js
-
-COPY "UnitTest\RunUnitTests.html" "build\RunUnitTests.html"
-COPY "UnitTest\RunUnitTests.css" "build\RunUnitTests.css"
